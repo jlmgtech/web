5e11d7cdd9211fe9d041dcca48de58a56b61673d
James Lay <james@eventrentalsystems.com>
2023-12-26 13:00:42 -0700

    write to client

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <uv.h>
 #include <errno.h>
@@ -9,6 +11,7 @@ void on_new_connection(uv_stream_t*, int);
 void on_read(uv_stream_t*, ssize_t, const uv_buf_t*);
 void alloc_buffer(uv_handle_t*, size_t, uv_buf_t*);
 void on_close(uv_handle_t*);
+void on_write_end(uv_write_t *req, int status);
 
 int main()
 {
@@ -19,6 +22,9 @@ int main()
     //server_on_data(server, on_data);
     //server_listen(server, "0.0.0.0", 8080);
 
+    const char *const addr_str = "0.0.0.0";
+    const int port = 8080;
+
     uv_loop_t *loop;
     uv_tcp_t server;
     struct sockaddr_in addr;
@@ -26,14 +32,23 @@ int main()
     loop = uv_default_loop();
     uv_tcp_init(loop, &server);
 
-    uv_ip4_addr("0.0.0.0", 8080, &addr);
+    uv_ip4_addr(addr_str, port, &addr);
     uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
 
     uv_listen((uv_stream_t*)&server, SOMAXCONN, on_new_connection);
 
+    printf("Listening on %s:%d\n", addr_str, port);
     uv_run(loop, UV_RUN_DEFAULT);
 
     return 0;
+}
+
+int cprintf(uv_stream_t *client, const char *fmt, ...);
+void dowrite(uv_stream_t *client, char *data, size_t len)
+{
+    uv_write_t *write_req = (uv_write_t*) malloc(sizeof(uv_write_t));
+    uv_buf_t write_buf = uv_buf_init(data, len);
+    uv_write(write_req, client, &write_buf, 1, on_write_end);
 }
 
 void onconnect(uv_stream_t *client)
@@ -53,13 +68,37 @@ void onclienterror(uv_stream_t *client, const char *error, const char *detail)
 
 void ondata(uv_stream_t *client, const char *data, size_t len)
 {
-    printf("client (%p) data(%lu): %s\n", (void*)client, len, data);
+    cprintf(client, "client (%p) data(%lu): %s\n", (void*)client, len, data);
 }
 
 void onclose(uv_handle_t *client)
 {
     printf("closing client %p\n", (void*)client);
 }
+
+int vacprintf(uv_stream_t *client, const char *fmt, va_list args)
+{
+    char *output = NULL;
+    int outlen = vasprintf(&output, fmt, args);
+    if (outlen <= 0) {
+        onclienterror(client, "failed allocation", strerror(errno));
+        free(output); // if output is NULL, free does nothing
+        return errno;
+    }
+    dowrite(client, output, outlen);
+    free(output);
+    return 0;
+}
+
+int cprintf(uv_stream_t *client, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    int ret = vacprintf(client, fmt, args);
+    va_end(args);
+    return ret;
+}
+
 
 
 // okay, so now we need to use the server pointer address as a key into a table of connections
@@ -86,18 +125,15 @@ void on_new_connection(uv_stream_t *server, int status)
     uv_tcp_init(uv_default_loop(), client);
 
     if (uv_accept(server, (uv_stream_t*) client) == 0) {
-
         onconnect((uv_stream_t*)client);
         uv_read_start((uv_stream_t*) client, alloc_buffer, on_read);
-
     } else {
-
         uv_close((uv_handle_t*) client, NULL);
-
     }
 }
 
-void alloc_buffer(__attribute__((unused)) uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+void alloc_buffer(__attribute__((unused)) uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
+{
     buf->base = (char*) malloc(suggested_size); // freed in on_read
     buf->len = suggested_size;
 }
@@ -106,9 +142,6 @@ void on_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
 {
     if (nread > 0) {
         ondata(client, buf->base, nread);
-        // Handle the data that was read
-        // buf->base contains the data
-        // nread is the number of bytes read
     } else if (nread < 0) {
         if (nread != UV_EOF) {
             // Error handling
@@ -120,6 +153,14 @@ void on_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
     if (buf->base) {
         free(buf->base); // Always free the buffer
     }
+}
+
+void on_write_end(uv_write_t *req, int status)
+{
+    if (status) {
+        onclienterror(req->handle, "write error", uv_err_name(status));
+    }
+    free(req);
 }
 
 void on_close(uv_handle_t* handle)
