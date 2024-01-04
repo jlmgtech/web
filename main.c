fc3dd9d6156d0c47d3392c25fff7960a26489387
james lay <james@eventrentalsystems.com>
2024-01-04 13:36:06 -0700

    add printf fxn 4 client

--- main.c
+++ main.c
@@ -6,7 +6,65 @@
 #include <errno.h>
 #include <string.h>
 
+
+
+// make client wrapper:
+// onconnect
+// write
+// ondata
+// close
+
+void on_server_close(uv_handle_t* handle);
 void on_server_connect(uv_connect_t* connection, int status);
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf);
+void process_read(uv_stream_t *server, ssize_t nread, const uv_buf_t *buf);
+
+void client_close(uv_connect_t* connection)
+{
+    uv_close((uv_handle_t*) connection->handle, on_server_close);
+}
+
+int client_vprintf(uv_stream_t *stream, char *fmt, va_list args)
+{
+    char *buffer = NULL;
+    int len = vasprintf(&buffer, fmt, args);
+    uv_buf_t buf = uv_buf_init(buffer, len);
+    uv_write_t request;
+    uv_write(&request, stream, &buf, 1, NULL);
+    free(buffer);
+    return len;
+}
+
+
+int client_printf(uv_stream_t *stream, char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    int len = client_vprintf(stream, fmt, args);
+    va_end(args);
+    return len;
+}
+
+
+void client_on_connect(uv_connect_t* connection)
+{
+    //printf("client_on_connect!\n");
+    client_printf(connection->handle, "Hi there!\n");
+}
+
+void client_on_error(uv_handle_t *handle, int error)
+{
+    fprintf(stderr, "Error: %s\n", uv_strerror(error));
+    uv_close(handle, on_server_close);
+}
+
+void client_on_data(uv_stream_t *server, char *buffer, ssize_t nread)
+{
+    printf("client_on_data!\n");
+    printf("nread: %lu\n", nread);
+    printf("buffer: %s\n", buffer);
+    //client_printf(server, "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n");
+}
 
 int main()
 {
@@ -26,42 +84,35 @@ int main()
     return 0;
 }
 
-//void connect(uv_loop_t *loop, char *ip, int port) {
-
-void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf);
-void echo_read(uv_stream_t *server, ssize_t nread, const uv_buf_t *buf);
-void on_server_connect(uv_connect_t* connection, int status) {
+void on_server_connect(uv_connect_t* connection, int status)
+{
     if (status < 0) {
         fprintf(stderr, "Connection error %s\n", uv_strerror(status));
-        // Handle the error
+        client_on_error(connection->handle, status);
         return;
     }
 
-    puts("successfully connected!\n");
-
-    // now write something:
-    char *buffer = "hello world\n";
-    uv_buf_t buf = uv_buf_init(buffer, strlen(buffer));
-    uv_write_t request;
-    uv_write(&request, connection->handle, &buf, 1, NULL);
-
-    // now receive the response:
-    uv_read_start(connection->handle, alloc_buffer, echo_read);
-
-    // Connection is successful
-    // You can start reading or writing data
+    client_on_connect(connection);
+    uv_read_start(connection->handle, alloc_buffer, process_read);
+}
+void on_server_close(uv_handle_t* handle)
+{
+    // if there's any cleanup, like the buffer, do it here
+    //printf("on_server_close!\n");
 }
 
-void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
+{
     *buf = uv_buf_init((char*) malloc(suggested_size), suggested_size);
 }
 
-void echo_read(uv_stream_t *server, ssize_t nread, const uv_buf_t *buf) {
+void process_read(uv_stream_t *server, ssize_t nread, const uv_buf_t *buf)
+{
     if (nread > 0) {
-        printf("Received data: %s\n", buf->base);
+        client_on_data(server, buf->base, nread);
     }
     else {
-        uv_close((uv_handle_t*) server, NULL);
+        client_on_error(server, nread);
     }
 
     // cargo culted from the docs:
